aaa1d421fbb35837df7f45c2e830a64e2b0dfdfa
Qizot <perzylo.jakub@gmial.com>
2019-11-07 13:32:08 +0100

    Navbar done, lost my mind, never again...

---
+++ scripts/navbar.js
@@ -0,0 +1,18 @@
+
+function toggleResponsive() {
+  var navbar = document.getElementById("navbar");
+  if (navbar.className === "navbar") {
+    navbar.className += " responsive";
+  } else {
+    navbar.className = "navbar";
+  }
+
+  var menu = document.getElementById("responsive-menu");
+  if (menu.className === "responsive-menu") {
+    menu.className += " sm-visible";
+  } else {
+    menu.className = "responsive-menu";
+  }
+
+
+}
